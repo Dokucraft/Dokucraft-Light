c8e5f3a77547b06576c73d45f2149c4629294103
Even Torset <eventorset@gmail.com>
2024-04-26 23:29:08 +0200

    Removed some leftover end sky code.

--- assets/minecraft/shaders/flavor.glsl
+++ assets/minecraft/shaders/flavor.glsl
@@ -16,25 +16,3 @@
 
 // Grass color multiplier for shader grass effects
 #define GRASS_COLOR_MULTIPLIER 1
-
-// Otherworldy Rift End sky colors
-#define END_SKY_RIFT_COLOR vec3(1.0, 0.1, 0.3)
-
-#define END_SKY_STARS_OUTSIDE_BASE_COLOR vec3(0.9, 0.6, 0.9)
-#define END_SKY_STARS_OUTSIDE_COLOR_VARIANCE vec3(0.1, 0.2, 0.1)
-#define END_SKY_NEBULAE_OUTSIDE_COLOR vec3(0.8, 0.1, 0.9)
-
-#define END_SKY_STARS_INSIDE_BASE_COLOR vec3(0.6, 1.2, 2.8)
-#define END_SKY_STARS_INSIDE_COLOR_VARIANCE vec3(0.1, 0.2, 0.1)
-#define END_SKY_NEBULAE_INSIDE_COLOR vec3(0.2, 0.5, 0.9)
-
-// Cloudy End sky colors
-#define END_SKY_2_BASE_COLOR vec3(0.05, 0.067, 0.133)
-
-#define END_SKY_2_WEAK_CLOUDS_COLOR vec3(0.501, 0.549, 0.741)
-#define END_SKY_2_STRONG_CLOUDS_PRIMARY_COLOR vec3(0.819, 0.839, 0.925)
-#define END_SKY_2_STRONG_CLOUDS_COLOR_1 vec3(0.6, 0.4, 1.2)
-#define END_SKY_2_STRONG_CLOUDS_COLOR_2 vec3(0.7, 0.8, 1.6)
-
-#define END_SKY_2_STARS_BASE_COLOR vec3(0.8, 0.8, 0.9)
-#define END_SKY_2_STARS_COLOR_VARIANCE vec3(0.1, 0.1, 0.1)
